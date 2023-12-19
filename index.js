c659c09325baafe80f4b0bde47ff8994839cb2d3
keshav <tomarkeshavofficial@gmail.com>
2023-12-19 17:49:04 +0530

    cors origin changed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://flexmoney-virid.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
